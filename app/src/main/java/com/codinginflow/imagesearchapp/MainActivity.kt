a457988d1bee66343a9f877f8e98f1f4ea5ddd0c
Artemiy Matchin <art16ws@gmail.com>
2021-06-27 18:19:14 +0300

    Fixed some warnings

--- app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.codinginflow.imagesearchapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
